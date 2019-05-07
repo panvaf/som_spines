84fa46fed4207a7ae2543267996e039682801fd2
panvaf <38217769+panvaf@users.noreply.github.com>
2019-05-07 13:01:38 +0200

    Continuous wavelet transform using FFT

---
+++ wavtrans.m
@@ -0,0 +1,25 @@
+function [real_trans, pseudofreq, scales] = wavtrans(record, chan, t, fsample, rowsPerOct, freqSpan, padmode, wavelet, plottype)
+% Calculate and show the wavelet transform for a single eeg channel
+%
+% eeg: 1x[time] Vector of eeg time series
+% fsample: sampling frequency
+% voicesPerOct: defines the transform's resolution in scales
+% freqSpan: frequency span to compute transform
+% padmode: see dwtmode for other choices
+% mwave: 'morl' (morlet) or 'mexh' (mexican hat)
+
+% Get adjusted scales
+scales= helperCWTTimeFreqVector(freqSpan(1),freqSpan(2),centfrq(wavelet),1/fsample,rowsPerOct);
+pseudofreq= mean([freqSpan(1)*scales(end),freqSpan(2)*scales(1)])./scales;
+
+% Transform
+trans= cwtft({record(chan,:),1/fsample},'wavelet',wavelet,'scales',scales,'padmode',padmode);
+real_trans= real(trans.cfs);
+
+% Plot
+figure
+wscalogram(plottype,flip(real_trans,1),'scales',flip(round(pseudofreq,2),2),'xdata',t,'ydata',record(chan,:));
+xlabel('Time (ms)'); ylabel('Freq (Hz)');
+title('Pseudofrequency-time transform');
+
+end
